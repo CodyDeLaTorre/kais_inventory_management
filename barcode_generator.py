7e64d4dbf0aa9d3779665241033adacf8b755b35
Cody De La Torre <103235685+CodyDeLaTorre@users.noreply.github.com>
2023-04-06 15:35:46 -0600

    Update barcode_generator.py

--- barcode_generator.py
+++ barcode_generator.py
@@ -11,3 +11,24 @@ while inventory_amount > 0:
     with open(f"testfile.svg{inventory_amount}", "wb") as f:
         EAN13(str(num), writer=SVGWriter()).write(f)
     inventory_amount = inventory_amount - 1
+
+    
+#_______BROKEN CODE______   
+# from io import BytesIO
+# import random
+# import os.path
+
+# from barcode import EAN13 , UPCA
+# from barcode .writer import SVGWriter
+
+# save_path = 'C:/home/n46252/dev/practice/inventory_management/barcodes/'
+
+# inventory_amount = 25
+
+# while inventory_amount > 0:
+#     name = f"testfile.svg{inventory_amount}"
+#     complete_name = os.path.join(save_path, name)
+#     num = random.randint(100000000000, 100000011111)
+#     with open(complete_name, "wb") as f:
+#         EAN13(str(num), writer=SVGWriter()).write(f)
+#     inventory_amount = inventory_amount - 1
