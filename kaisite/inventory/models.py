30dd080b6aebffbeb9fa54f61c99c47acb0abfc4
Cody De La Torre <103235685+CodyDeLaTorre@users.noreply.github.com>
2023-04-07 13:19:40 -0600

    Update models.py

--- kaisite/inventory/models.py
+++ kaisite/inventory/models.py
@@ -4,10 +4,12 @@ from django.urls import reverse
 
 class Item(models.Model):
     name = models.CharField(max_length=256)
+    description = models.TextField("")
+    picker = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    barcode_number = models.IntegerField()
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse("item_detail", args=[str(self.id)])
-    
